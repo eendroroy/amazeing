66f48d34d7e0421b6e54abac1fa635b955e45703
indrajit <eendroroy@gmail.com>
2025-02-25 12:07:12 +0600

    add bfs and dfs simulation

--- amazeing-app/src/bsf.rs
+++ amazeing-app/src/bsf.rs
@@ -1,8 +1,10 @@
 use amazeing_core::square::{bfs, Maze};
 use macroquad::miniquad::window::set_window_size;
 use macroquad::prelude::*;
+use std::thread::sleep;
+use std::time::Duration;
 
-#[macroquad::main("Maze Solver")]
+#[macroquad::main("Maze Solver (BFS)")]
 async fn main() {
     let maze = Maze::from([
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -38,15 +40,9 @@ async fn main() {
         [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
     ]);
 
-    let (from, to) = ((5, 0), (30, 9));
-    let path = bfs(&maze, from, to);
-
-    // let maze = Maze::from([
-    //     [0, 0, 0, 0],
-    //     [0, 0, 1, 0],
-    //     [0, 0, 0, 0],
-    //     [0, 0, 0, 0],
-    // ]);
+    let (from, to) = ((5, 0), (29, 30));
+    let mut tracer: Option<Vec<Vec<(usize, usize)>>> = Some(vec![]);
+    bfs(&maze, from, to, &mut tracer);
 
     let (margin, padding) = (20., 3.);
     let (maze_width, maze_height) = (maze.cols(), maze.rows());
@@ -58,29 +54,55 @@ async fn main() {
 
     set_window_size(screen_width as u32, screen_height as u32 + 30);
 
+    let data = tracer.clone().unwrap();
+    let mut solved = false;
+
     loop {
-        clear_background(BLACK);
+        if solved {
+            draw(maze, margin, padding, cell_width, cell_height, data.last().unwrap().clone(), GOLD);
+            next_frame().await
+        } else {
+            for i in 0..data.len() {
+                if i == data.len() - 1 {
+                    solved = true
+                }
 
-        for r in 0..maze.rows() {
-            for c in 0..maze.cols() {
-                let color: Color = if path.contains(&(r, c)) {
-                    GOLD
-                } else if maze[(r, c)] > 0 {
-                    LIGHTGRAY
-                } else {
-                    DARKGRAY
-                };
-
-                draw_rectangle(
-                    margin + c as f32 * (cell_width + padding),
-                    margin + r as f32 * (cell_height + padding),
-                    cell_width,
-                    cell_height,
-                    color,
-                );
+                sleep(Duration::from_millis(150));
+                draw(maze, margin, padding, cell_width, cell_height, data[i].clone(), MAGENTA);
+                next_frame().await
             }
         }
+    }
+}
 
-        next_frame().await
+fn draw<const ROWS: usize, const COLS: usize>(
+    maze: Maze<ROWS, COLS>,
+    margin: f32,
+    padding: f32,
+    cell_width: f32,
+    cell_height: f32,
+    path: Vec<(usize, usize)>,
+    path_color: Color,
+) {
+    clear_background(BLACK);
+
+    for r in 0..maze.rows() {
+        for c in 0..maze.cols() {
+            let color: Color = if path.contains(&(r, c)) {
+                path_color
+            } else if maze[(r, c)] > 0 {
+                LIGHTGRAY
+            } else {
+                DARKGRAY
+            };
+
+            draw_rectangle(
+                margin + c as f32 * (cell_width + padding),
+                margin + r as f32 * (cell_height + padding),
+                cell_width,
+                cell_height,
+                color,
+            );
+        }
     }
 }
