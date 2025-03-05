694531ad969111a2d0b3638af9bfb35f32b27ab4
indrajit <eendroroy@gmail.com>
2025-03-05 19:21:04 +0600

    add option to block opened path

--- amazeing-generator/src/manual/draw.rs
+++ amazeing-generator/src/manual/draw.rs
@@ -1,7 +1,7 @@
 use crate::{BG, BLOCK, OPEN};
 use amazeing::solver::matrix::Maze;
 use macroquad::color::Color;
-use macroquad::input::{is_key_pressed, KeyCode};
+use macroquad::input::{is_key_down, is_key_pressed, KeyCode};
 use macroquad::prelude::{clear_background, draw_rectangle, is_mouse_button_pressed, mouse_position, next_frame, MouseButton};
 
 pub(crate) fn draw(maze: &Maze, margin: f32, padding: f32, cell_width: f32, cell_height: f32) {
@@ -35,13 +35,19 @@ pub(crate) async fn looper(
         }
 
         if is_mouse_button_pressed(MouseButton::Left) {
+            let value = if is_key_down(KeyCode::LeftShift) || is_key_down(KeyCode::RightShift) {
+                0
+            } else {
+                1
+            };
+
             let (mx, my) = mouse_position();
             let r = ((my - margin)/(cell_height + padding)).floor();
             let c = ((mx - margin)/(cell_width + padding)).floor();
 
-            maze[(r as usize, c as usize)] = 1;
-
+            maze[(r as usize, c as usize)] = value;
         }
+
         draw(maze, margin, padding, cell_width, cell_height);
         next_frame().await
     }
