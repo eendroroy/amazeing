a990cc61b5f0c52ca6a1a669c7d876f6544f4458
indrajit <eendroroy@gmail.com>
2025-03-09 15:56:49 +0600

    bfs dfs maze generator

---
+++ amazeing/src/generator/matrix/traverse.rs
@@ -0,0 +1,44 @@
+use crate::helper::validate_node;
+use crate::maze::matrix::{neighbours_block, Maze, D, L, R, U};
+use crate::structure::queue::Queue;
+use crate::structure::stack::Stack;
+use crate::structure::structure_traits::DataStorage;
+use crate::structure::DNode;
+use std::collections::BTreeMap;
+
+fn traverse(
+    maze: &mut Maze,
+    source: DNode,
+    storage: &mut dyn DataStorage<DNode>,
+    _tracer: &mut Option<Vec<Vec<DNode>>>,
+) -> Vec<DNode> {
+    validate_node(maze, source);
+
+    let mut parent: BTreeMap<DNode, DNode> = BTreeMap::new();
+
+    storage.push(source);
+
+    while let Some(current) = storage.pop() {
+        let neighbours = neighbours_block(maze, current, Some(vec![L, R, U, D]));
+
+        if neighbours.len() > 2 {
+            maze[current] = 1;
+            for next in neighbours {
+                parent.insert(next, current);
+                storage.push(next);
+            }
+        }
+    }
+
+    Vec::new()
+}
+
+pub fn bfs(maze: &mut Maze, start: DNode, tracer: &mut Option<Vec<Vec<DNode>>>) -> Vec<DNode> {
+    let mut queue = Queue::<DNode>::new();
+    traverse(maze, start, &mut queue, tracer)
+}
+
+pub fn dfs(maze: &mut Maze, start: DNode, tracer: &mut Option<Vec<Vec<DNode>>>) -> Vec<DNode> {
+    let mut queue = Stack::<DNode>::new();
+    traverse(maze, start, &mut queue, tracer)
+}
