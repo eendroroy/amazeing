4c9f3e0f29c86bcc84ef0e7e7227c9b77890edee
indrajit <eendroroy@gmail.com>
2025-03-12 19:20:52 +0600

    fix visualize mode

--- amazeing-bin/src/mode/visualize.rs
+++ amazeing-bin/src/mode/visualize.rs
@@ -1,31 +1,27 @@
 use crate::context::{DRAW_CTX, VIS_CTX};
 use crate::display::action::quit_requested;
 use crate::display::drawer::draw_maze;
-use crate::helper::loader::loader_maze_from_file;
-use amazeing::maze::matrix::Maze;
 use macroquad::miniquad::window::set_window_size;
 use macroquad::prelude::*;
 
-async fn looper(maze: &mut Maze) {
+async fn looper() {
     loop {
         if quit_requested() {
             break;
         }
 
-        draw_maze(maze);
+        draw_maze(&VIS_CTX.read().unwrap().maze);
         next_frame().await
     }
 }
 
 #[macroquad::main("Maze Viewer")]
 async fn main() {
-    let mut maze = loader_maze_from_file(&*VIS_CTX.read().unwrap().maze_file_path);
-
     let (screen_width, screen_height) = DRAW_CTX.read().unwrap().screen_size();
 
     set_window_size(screen_width as u32, screen_height as u32 + 30);
 
-    looper(&mut maze).await
+    looper().await
 }
 
 pub(crate) fn visualize() {
