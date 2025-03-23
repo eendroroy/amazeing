6054f6cf0255bae1c03550f157cc56f7ff1015e6
indrajit <eendroroy@gmail.com>
2025-03-23 17:02:51 +0600

    update modules

--- amazeing-bin/src/context/mod.rs
+++ amazeing-bin/src/context/mod.rs
@@ -5,10 +5,9 @@ mod generation_context;
 mod solve_context;
 mod visualize_context;
 
-pub(crate) use color_context::COLOR_CTX;
-pub(crate) use color_context::ColorContext;
-pub(crate) use color_scheme::ColorScheme;
-pub(crate) use draw_context::DRAW_CTX;
-pub(crate) use generation_context::GEN_CTX;
-pub(crate) use solve_context::SOLVE_CTX;
-pub(crate) use visualize_context::VIS_CTX;
+pub(crate) use color_context::*;
+pub(crate) use color_scheme::*;
+pub(crate) use draw_context::*;
+pub(crate) use generation_context::*;
+pub(crate) use solve_context::*;
+pub(crate) use visualize_context::*;
