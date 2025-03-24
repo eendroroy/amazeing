2b7210d11883d95b89c98e553c79f7230ebdbc8d
indrajit <eendroroy@gmail.com>
2025-03-24 17:37:58 +0600

    reorganize modules

--- amazeing-bin/src/main.rs
+++ amazeing-bin/src/main.rs
@@ -1,19 +1,17 @@
 use crate::command::ArgMode::{Create, Solve, View};
 use crate::command::{update_context, AmazeingArgs};
 use crate::context::DRAW_CTX;
-use crate::ui::generate_loop::generate_loop;
-use crate::ui::generate_simulate::generate_simulation_loop;
-use crate::ui::update::update_loop;
-use crate::ui::view::view_loop;
+use crate::display_loop::{
+    generate_loop, generate_simulation_loop, solve_loop, solve_simulation_loop, update_loop,
+    view_loop,
+};
 use clap::Parser;
 use macroquad::miniquad::window::set_window_size;
-use crate::ui::solve::solve_loop;
-use crate::ui::solve_simulate::solve_simulation_loop;
 
 mod command;
 mod context;
+mod display_loop;
 mod helper;
-mod ui;
 
 #[macroquad::main("Amazeing")]
 async fn main() {
--- amazeing-bin/src/ui/mod.rs
+++
@@ -1,6 +0,0 @@
-pub(crate) mod generate_loop;
-pub(crate) mod generate_simulate;
-pub(crate) mod solve;
-pub(crate) mod solve_simulate;
-pub(crate) mod update;
-pub(crate) mod view;
