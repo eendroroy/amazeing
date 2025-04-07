1745ad3fa298cee82d293d7133aa5580a64027f8
indrajit <eendroroy@gmail.com>
2025-04-07 20:00:31 +0600

    refactor

--- amazeing/src/helper/mod.rs
+++ amazeing/src/helper/mod.rs
@@ -2,7 +2,7 @@ mod draw_maze;
 mod file;
 mod get_node;
 mod path_to_trace;
-mod read_source_destination;
+mod read_node;
 mod run_algorithm;
 mod time;
 
@@ -10,6 +10,6 @@ pub(crate) use draw_maze::*;
 pub(crate) use file::*;
 pub(crate) use get_node::*;
 pub(crate) use path_to_trace::*;
-pub(crate) use read_source_destination::*;
+pub(crate) use read_node::*;
 pub(crate) use run_algorithm::*;
 pub(crate) use time::*;
