d524a8bb1bae9419406aa2ca1e08f0c0ad6147ad
indrajit <eendroroy@gmail.com>
2025-04-10 19:58:27 +0600

    add support regeneration of maze

--- amazeing/src/display_loop/generate_loop.rs
+++ amazeing/src/display_loop/generate_loop.rs
@@ -6,18 +6,21 @@ use macroquad::prelude::*;
 pub(crate) async fn generate_loop(context: &CreateContext, draw_context: &DrawContext, color_context: &ColorContext) {
     let mut maze = Maze::from(vec![vec![0u32; context.cols]; context.rows]);
 
-    generate_maze(&mut maze, &draw_context.shape, context.sources.clone(), &context.procedure, &mut None);
-
-    if let Some(maze_file_path) = context.maze_file_path.clone() {
-        dump_maze_to_file(&maze_file_path, &maze);
-    }
-
     loop {
         if is_key_pressed(KeyCode::Q) {
             break;
         }
 
-        draw_maze(draw_context, color_context, &maze, None, None, (vec![], None), false);
+        if is_key_pressed(KeyCode::G) || is_key_pressed(KeyCode::Space) {
+            maze = Maze::from(vec![vec![0u32; context.cols]; context.rows]);
+            generate_maze(&mut maze, &draw_context.shape, context.sources.clone(), &context.procedure, &mut None);
+        }
+
+        draw_maze(draw_context, color_context, &maze, None, None, (context.sources.clone(), None), false);
         next_frame().await
+    }
+
+    if let Some(maze_file_path) = context.maze_file_path.clone() {
+        dump_maze_to_file(&maze_file_path, &maze);
     }
 }
